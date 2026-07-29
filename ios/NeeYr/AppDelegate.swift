4fe71b6ce9f5b1c8b8d25e77149bcfcd7f0b6017
Zayar Naing <zayarnaing.st@gmail.com>
2026-07-29 08:44:22 +0630

    app_name

--- ios/NeeYr/AppDelegate.swift
+++ ios/NeeYr/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "NeeYr Directory",
+      withModuleName: "NeeYr",
       in: window,
       launchOptions: launchOptions
     )
